05ce84559f9a7a82ecaf4fa4972d2c2d54d775bd
Carl Joachim Rorvik <carljoac@gmail.com>
2013-10-15 14:10:53 +0200

    changed url

--- public/js/Models/ControllerModel.js
+++ public/js/Models/ControllerModel.js
@@ -11,8 +11,8 @@
 		players: [],
 		numberOfPlayers: 0,
 		initialize: function(){
-			socket = io.connect("78.91.68.120", {port: 8080, transports: ["websocket"]});
-			//socket = io.connect("192.168.1.4", {port: 8080, transports: ["websocket"]});
+			//socket = io.connect("78.91.68.120", {port: 8080, transports: ["websocket"]});
+			socket = io.connect("http://ec2-54-229-164-44.eu-west-1.compute.amazonaws.com", {port: 8080, transports: ["websocket"]});
 
 			this.setEventHandlers(socket);	
 
