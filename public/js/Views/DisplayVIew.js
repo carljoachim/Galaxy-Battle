fb4ba67db4b74bcb6e755d37035226e0f051b3f7
Carl Joachim Rorvik <carljoac@gmail.com>
2013-10-15 14:28:58 +0200

    bug

--- public/js/Views/DisplayVIew.js
+++ public/js/Views/DisplayVIew.js
@@ -25,6 +25,8 @@
 			this.model = options.model;			
 			this.el.html(this.introTemplate);
 
+			console.log("display");
+
 			Simple.Events.on("display:game-generated", this.showGameCode);			
 			Simple.Events.on("display:player-joined", this.renderPlayersList);
 			Simple.Events.on("display:start-game", this.startGame.bind(this));			
